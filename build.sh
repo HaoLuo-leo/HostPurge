06e984552f3d1d6f2b3b8185109d1b9506cff8ba
HaoLuo-leo <138950844+HaoLuo-leo@users.noreply.github.com>
2024-07-29 12:20:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ mkdir -p $PREFIX/bin
 
 # Copy necessary Python scripts to the bin directory
 cp $SRC_DIR/build_db.py $PREFIX/bin
-cp $SRC_DIR/combined_mode.py $PREFIX/bin
 cp $SRC_DIR/kneaddata_mode.py $PREFIX/bin
 cp $SRC_DIR/kraken2_mode.py $PREFIX/bin
-cp $SRC_DIR/main.py $PREFIX/bin
+cp $SRC_DIR/hostpurge.py $PREFIX/bin
 
 # Optionally, if there are any directories to be copied and compiled
 # cp -rf $SRC_DIR/some_directory $PREFIX/bin
@@ -17,10 +16,5 @@ cp $SRC_DIR/main.py $PREFIX/bin
 # cd $PREFIX/bin/some_directory
 # make
 
-# Install kraken2
-conda install kraken2=2.1.2 -y
-
-# Install kneaddata
-conda install kneaddata=0.12.0 -y 
 
 
